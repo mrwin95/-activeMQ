7d25173c874fb94d6493b1d1458fdc9ea20d09c9
Thang Nguyen <mrwin95@gmail.com>
2025-08-30 17:24:32 -0600

    poison message pattern

--- backend/week-2/shop-app-wk2/notification-service/src/main/java/activemq/notificationsvc/messaging/NotificationListener.java
+++ backend/week-2/shop-app-wk2/notification-service/src/main/java/activemq/notificationsvc/messaging/NotificationListener.java
@@ -2,7 +2,7 @@ package activemq.notificationsvc.messaging;
 
 import activemq.dto.OrderCreatedMessage;
 import activemq.notificationsvc.config.JmsTopicConfig;
-//import activemq.notificationsvc.domain.OrderCreatedMessage;
+import org.apache.activemq.artemis.api.core.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
@@ -13,13 +13,29 @@ public class NotificationListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(NotificationListener.class);
 
-    @JmsListener(destination = JmsTopicConfig.NOTIFICATIONS_TOPIC, subscription = "${app.jms.subscription_name}", containerFactory = "topicJmsListenerContainerFactory")
-    public void onMessage(OrderCreatedMessage msg) {
+    @JmsListener(destination = JmsTopicConfig.NOTIFICATIONS_TOPIC,
+            subscription = "${app.jms.subscription_name}",
+            containerFactory = "topicJmsListenerContainerFactory")
+    public void onMessage(OrderCreatedMessage msg, Message jms) throws Exception {
+        int attempt = jms.getIntProperty("JMSXDeliveryCount"); // 1 on first try
+        String key = jms.getStringProperty("eventKey"); // if producer sets it
         try {
-            LOG.info("[Notification] orderId={}, customer={}, total={}",
-                    msg.orderId(), msg.customerName(), msg.total());
+            // -- do the work --
+            LOG.info("consume orderCreated key={} attempt={} id={} customer={} total={}",
+                    key, attempt, msg.orderId(), msg.customerName(), msg.total());
+
+//            LOG.info("[Notification] orderId={}, customer={}, total={}",
+//                    msg.orderId(), msg.customerName(), msg.total());
+        }catch (IllegalArgumentException bad){
+            LOG.warn("poison message (non-recoverable), sending to DLQ now. key={}", key, bad);
+            throw bad;
         }catch (Exception e){
-            LOG.error("Unable to process message: {}", msg , e);
+            if (attempt >= 5) {
+                LOG.error("giving up after {} attempts; will DLQ. key={}", attempt, key, e);
+            }else {
+                LOG.warn("transient failure (attempt {}), will redeliver. key={}",attempt, key, e);
+            }
+            throw e;
         }
 
     }
